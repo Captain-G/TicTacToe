738f41c5ce30286d3952520cbacf79e088e99319
Captian-G <poppinpites7@gmail.com>
2022-11-20 20:48:59 +0300

    Python TicTacToe with GUI

--- main.py
+++ main.py
@@ -12,8 +12,17 @@ cross_img = pygame.image.load("x.png")
 nought_img = pygame.image.load("o.png")
 
 counter = 1
-this = 0
+is_even = False
 
+position_1 = False
+position_2 = False
+position_3 = False
+position_4 = False
+position_5 = False
+position_6 = False
+position_7 = False
+position_8 = False
+position_9 = False
 
 
 def cross(x, y):
@@ -24,12 +33,12 @@ def nought(x, y):
     screen.blit(nought_img, (x, y))
 
 
-def draw(count, x, y):
-    count += 1
-    if count % 2 != 0:
+def draw(isEven, x, y):
+    if isEven:
         screen.blit(nought_img, (x, y))
     else:
         screen.blit(cross_img, (x, y))
+    return isEven
 
 
 running = True
@@ -40,6 +49,25 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_KP7:
+                position_1 = True
+            elif event.key == pygame.K_KP8:
+                position_2 = True
+            elif event.key == pygame.K_KP9:
+                position_3 = True
+            elif event.key == pygame.K_KP4:
+                position_4 = True
+            elif event.key == pygame.K_KP5:
+                position_5 = True
+            elif event.key == pygame.K_KP6:
+                position_6 = True
+            elif event.key == pygame.K_KP1:
+                position_7 = True
+            elif event.key == pygame.K_KP2:
+                position_8 = True
+            elif event.key == pygame.K_KP3:
+                position_9 = True
 
     pygame.draw.line(screen, (0, 0, 0), (165, 0), (165, 450), width=3)
     pygame.draw.line(screen, (0, 0, 0), (330, 0), (330, 450), width=3)
@@ -47,63 +75,41 @@ while running:
     pygame.draw.line(screen, (0, 0, 0), (0, 150), (500, 150), width=3)
     pygame.draw.line(screen, (0, 0, 0), (0, 300), (500, 300), width=3)
 
-    mouse_x, mouse_y = pygame.mouse.get_pos()
-    left, middle, right = pygame.mouse.get_pressed()
-
-    position = 0
-    if mouse_x < 165 and mouse_y < 150:
-        position = 1
-    elif mouse_x < 330 and mouse_y < 150:
-        position = 2
-    elif mouse_x < 495 and mouse_y < 150:
-        position = 3
-    elif mouse_x < 165 and mouse_y < 300:
-        position = 4
-    elif mouse_x < 330 and mouse_y < 300:
-        position = 5
-    elif mouse_x < 495 and mouse_y < 300:
-        position = 6
-    elif mouse_x < 165 and mouse_y > 300:
-        position = 7
-    elif mouse_x < 330 and mouse_y > 300:
-        position = 8
-    elif mouse_x > 330 and mouse_y > 300:
-        position = 9
-    else:
-        position = 0
-
-    is_clicked = left
-
-    if position == 1 and is_clicked:
-        this = 1
-    elif position == 2 and is_clicked:
-        this = 2
-        draw(counter, 183.5, 11)
-    elif position == 3 and is_clicked:
-        this = 3
-        draw(counter, 348.5, 11)
-    if position == 4 and is_clicked:
-        this = 4
-        screen.blit(nought_img, (18.5, 161))
-    if position == 5 and is_clicked:
-        this = 5
-        screen.blit(nought_img, (183.5, 161))
-    if position == 6 and is_clicked:
-        this = 6
-        screen.blit(nought_img, (348.5, 161))
-    if position == 7 and is_clicked:
-        this = 7
-        screen.blit(nought_img, (18.5, 311))
-    if position == 8 and is_clicked:
-        this = 8
-        screen.blit(nought_img, (183.5, 311))
-    if position == 9 and is_clicked:
-        this = 9
-        screen.blit(nought_img, (348.5, 311))
-
-    if this == 1:
-        draw(counter, 18.5, 11)
-    if this == 2:
-        draw(counter, 183.5, 11)
+    if position_1:
+        draw(is_even, 5, 11)
+        if is_even:
+            is_even = False
+        else:
+            is_even = True
+    if position_2:
+        draw(is_even, 183.5, 11)
+        if is_even:
+            is_even = False
+        else:
+            is_even = True
+    if position_3:
+        draw(is_even, 348.5, 11)
+        if is_even:
+            is_even = False
+        else:
+            is_even = True
+    if position_4:
+        draw(counter, 18.5, 161)
+        counter += 1
+    if position_5:
+        draw(counter, 183.5, 161)
+        counter += 1
+    if position_6:
+        draw(counter, 348.5, 161)
+        counter += 1
+    if position_7:
+        draw(counter, 18.5, 311)
+        counter += 1
+    if position_8:
+        draw(counter, 183.5, 311)
+        counter += 1
+    if position_9:
+        draw(counter, 348.5, 311)
+        counter += 1
 
     pygame.display.update()
