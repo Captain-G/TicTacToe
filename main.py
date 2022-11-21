91b6584890a7f2bdcc1363bba0ed82fe07f324d8
Captian-G <poppinpites7@gmail.com>
2022-11-21 22:29:12 +0300

    Python TicTacToe with GUI

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 
 pygame.init()
+clock = pygame.time.Clock()
 
 screen = pygame.display.set_mode((500, 450))
 
@@ -12,6 +13,7 @@ cross_img = pygame.image.load("x.png")
 nought_img = pygame.image.load("o.png")
 
 counter = 1
+presses = 1
 is_even = False
 isBool = True
 
@@ -35,14 +37,11 @@ def nought(x, y):
 
 
 def draw(x, y):
-    global is_even  # is_even is False
-    global isBool
-    if is_even:
+    global presses
+    if presses % 2 == 0:
         screen.blit(nought_img, (x, y))
-        is_even = False
-    elif not is_even:
+    else:
         screen.blit(cross_img, (x, y))
-        is_even = True
 
 
 running = True
@@ -56,22 +55,31 @@ while running:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_KP7:
                 position_1 = True
+                presses += 1
             elif event.key == pygame.K_KP8:
                 position_2 = True
+                presses += 1
             elif event.key == pygame.K_KP9:
                 position_3 = True
+                presses += 1
             elif event.key == pygame.K_KP4:
                 position_4 = True
+                presses += 1
             elif event.key == pygame.K_KP5:
                 position_5 = True
+                presses += 1
             elif event.key == pygame.K_KP6:
                 position_6 = True
+                presses += 1
             elif event.key == pygame.K_KP1:
                 position_7 = True
+                presses += 1
             elif event.key == pygame.K_KP2:
                 position_8 = True
+                presses += 1
             elif event.key == pygame.K_KP3:
                 position_9 = True
+                presses += 1
 
     pygame.draw.line(screen, (0, 0, 0), (165, 0), (165, 450), width=3)
     pygame.draw.line(screen, (0, 0, 0), (330, 0), (330, 450), width=3)
@@ -114,5 +122,5 @@ while running:
     #     print("Player wins")
     # elif position_1 and position_5 and position_9:
     #     print("Player wins")
-
+    clock.tick(30)
     pygame.display.update()
